70e1996021318287f6136ec1925bbc18d7de1bb3
takana-v <44311840+takana-v@users.noreply.github.com>
2021-12-12 00:08:07 +0900

    fix SynthesisEngine

--- voicevox_engine/synthesis_engine/__init__.py
+++ voicevox_engine/synthesis_engine/__init__.py
@@ -1,10 +1,8 @@
-from .forwarder import Forwarder
+from .coeiroink_engine import SynthesisEngine
 from .make_synthesis_engine import make_synthesis_engine
-from .synthesis_engine import SynthesisEngine
 from .synthesis_engine_base import SynthesisEngineBase
 
 __all__ = [
-    "Forwarder",
     "make_synthesis_engine",
     "SynthesisEngine",
     "SynthesisEngineBase",
